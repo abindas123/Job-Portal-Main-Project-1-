a29f0603f2cf6e96e4a7c2b7545aed17ede02657
abindas123 <abin9111re@gmail.com>
2026-03-19 14:50:21 +0100

    updated cors

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-   origin: "https://your-frontend-url.vercel.app",
+    origin: "https://job-portal-main-project-1.vercel.app",
   credentials: true,
 }));
 
